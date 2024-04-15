9f958e017d2f57658a987a81cb18c1db78a1f939
alexlanxy <33140761+alexlanxy@users.noreply.github.com>
2024-04-14 21:38:56 -0500

    modify the index

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
-import Link from 'next/link';
-import React from 'react';
-import Layout from './layout';
-const Page = () => {
-  return (
-    <div>
-      <Layout>
+
+// pages/search.js
+'use client'
+import SearchForm from '../components/SearchForm';
+import SearchResults from '../components/SearchResults';
+import { useState } from 'react';
+
+export default function SearchPage() {
+    const [results, setResults] = useState([]);
+
+    const handleSearch = async (query : any) => {
+        const externalApiUrl = `https://api.artic.edu/api/v1/artworks/search?q=${encodeURIComponent(query)}`;
+        const res = await fetch(externalApiUrl);
+        const data = await res.json();
+        setResults(data.data); // Adjust this line based on the actual structure of the response
+    };
+
+    return (
         <div>
-          <h1>Welcome to the Page</h1>
-          <p>This is a sample page that links to the search page.</p>
-          <Link href="/search">
-
-            <a>Go to Search Page</a>
-          </Link>
+            <SearchForm onSearch={handleSearch} />
+            <SearchResults results={results} />
         </div>
-      </Layout>
-      </div>
-  );
-};
-
-
-
-export default Page;
+    );
+}
